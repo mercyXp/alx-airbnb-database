a1bdfdc5341965ab3894630216a7ad1219890ae0
MercyXp <munzenzimercy2024@gmail.com>
2025-07-13 15:55:55 +0200

    database index added

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,7 +1,34 @@
--- Indexes for frequently used columns
+-- Measure performance BEFORE creating indexes
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = '00000000-0000-0000-0000-000000000001';
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = '00000000-0000-0000-0000-000000000002';
+
+EXPLAIN ANALYZE
+SELECT * FROM Property WHERE host_id = '00000000-0000-0000-0000-000000000003';
+
+EXPLAIN ANALYZE
+SELECT * FROM User WHERE email = 'example@example.com';
+
+-- Create indexes to optimize queries
+
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_user_email ON User(email);
-CREATE INDEX idx_booking_user ON Booking(user_id);
-CREATE INDEX idx_booking_property ON Booking(property_id);
-CREATE INDEX idx_review_property ON Review(property_id);
-CREATE INDEX idx_property_location ON Property(location);
-CREATE INDEX idx_booking_dates ON Booking(start_date);
+
+-- Measure performance AFTER creating indexes
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = '00000000-0000-0000-0000-000000000001';
+
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE property_id = '00000000-0000-0000-0000-000000000002';
+
+EXPLAIN ANALYZE
+SELECT * FROM Property WHERE host_id = '00000000-0000-0000-0000-000000000003';
+
+EXPLAIN ANALYZE
+SELECT * FROM User WHERE email = 'example@example.com';
